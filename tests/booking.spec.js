b1edbb873a7b3e46e4ef23240cdabe4958444758
Sameer <imacm1@iMac-iMac.local>
2026-03-02 11:29:55 +0400

    Updated test file name

--- tests/booking.spec.js
+++ tests/booking.spec.js
@@ -1,6 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
 test('Google opens and title is correct', async ({ page }) => {
+  // Google səhifəsini aç
   await page.goto('https://google.com');
+
+  // Başlığı yoxla
   await expect(page).toHaveTitle(/Google/);
 });
